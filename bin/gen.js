0ab866987ea1ba3b9062ec5e20796c64d27f148e
quatrix <eddie@refundit.tax>
2017-12-12 17:42:07 +0200

    now with types for success

--- bin/gen.js
+++ bin/gen.js
@@ -1,24 +1,52 @@
 const YAML = require('yamljs')
 const fs = require('fs')
 const CodeGen = require('swagger-js-codegen').CodeGen
+const path = require('path')
+const _ = require('lodash')
 
 const file = process.argv[2]
-const out = process.argv[3]
+const outdir = process.argv[3]
 
 const swaggerSpec = YAML.load(file)
 
-var source = CodeGen.getCustomCode({
-  moduleName: 'Sample',
-  className: 'Actions',
-  lint: false,
-  beautify: false,
-  swagger: swaggerSpec,
-  template: {
-    class: fs.readFileSync('templates/class.mustache', 'utf-8'),
-    method: fs.readFileSync('templates/method.mustache', 'utf-8'),
-    type: fs.readFileSync('templates/type.mustache', 'utf-8')
-  }
+const tags = new Set()
+
+_.map(swaggerSpec.paths, p => {
+  _.map(p, m => {
+    _.map(m.tags, t => {
+      tags.add(t)
+    })
+  })
 })
 
-console.log(source)
-fs.writeFileSync(out, source)
+for (const tag of tags) {
+  const spec = _.cloneDeep(swaggerSpec)
+
+  for (const path of Object.keys(spec.paths)) {
+    for (const method of Object.keys(spec.paths[path])) {
+      if (!_.includes(spec.paths[path][method].tags, tag)) {
+        delete spec.paths[path][method]
+      }
+    }
+
+    if (!Object.keys(spec.paths[path]).length) {
+      delete spec.paths[path]
+    }
+  }
+
+  const source = CodeGen.getCustomCode({
+    moduleName: 'Sample',
+    className: 'API//'+tag,
+    lint: false,
+    beautify: false,
+    swagger: spec,
+    template: {
+      class: fs.readFileSync('templates/class.mustache', 'utf-8'),
+      method: fs.readFileSync('templates/method.mustache', 'utf-8'),
+      type: fs.readFileSync('templates/type.mustache', 'utf-8')
+    }
+  })
+
+  console.log(source)
+  fs.writeFileSync(path.join(outdir, tag + '.ts'), source)
+}
--- dist/use.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Sample = require("./a");
-console.log(Sample);
-//# sourceMappingURL=use.js.map
